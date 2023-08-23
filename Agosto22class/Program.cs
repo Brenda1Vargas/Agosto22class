151f28e02870831bc4623db2a336cd762507ede1
Brenda1Vargas <brendal.vargasp@konradlorenz.edu.co>
2023-08-23 15:10:56 -0500

    Brenda1Vargas
    ejercicio3

--- Agosto22class/Program.cs
+++ Agosto22class/Program.cs
@@ -5,6 +5,11 @@
         static void Main(string[] args)
         {
             //menu para seleccionar el programa a ejecutar
+            Console.WriteLine("****************BIENVENIDO****************");
+            Console.WriteLine("************Seleccione una opcion*****");
+            Console.WriteLine("1. Promedio de 3 números");
+            Console.WriteLine("2. Área y perímetro de un rectángulo");
+            Console.WriteLine("3. Volumen de un cilindro");
             int op;
             op = int.Parse(Console.ReadLine());
 
@@ -19,7 +24,12 @@
                     exercise2.ExecuteExercise2();
                     break;
                 case 3:
-                    Console.WriteLine("CASO 3");
+                    Console.WriteLine("VOLUMEN DE UN CILINDRO");
+                    exercise3.ExecuteExercise3();
+                    break;
+
+                    case 4:
+                    Console.WriteLine("numero inválido, ingrese una opción del 1-3");
                     break;
 
             }
---
+++ Agosto22class/exercise3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agosto22class
+{
+    internal class exercise3
+    {
+        public static void ExecuteExercise3()
+        {
+            Console.WriteLine("ingrese el radio del cilindro");
+            int rad = 0;
+            rad = int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese la altura del cilindro");
+            int alt = 0;
+            alt = int.Parse(Console.ReadLine());
+
+            if (rad < 0)
+            {
+                Console.WriteLine("numero negativo, por favor ingrese un entero positivo");
+            } else
+            {
+                double Vo = 0;
+                double k = 3.14 ;
+                Vo = (k * ((rad) ^ 2 )* alt);
+                Console.WriteLine("el volumen del cilindro es: "+ Vo);
+            }
+        }
+    }
+}
